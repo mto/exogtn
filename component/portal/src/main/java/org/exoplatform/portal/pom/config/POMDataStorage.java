abb43a1efb421ee0cca29debe5b025b44ac655d8
Julien Viet <julien@julienviet.com>
2011-03-15 17:35:34 +0100

    implement lazy list access

--- component/portal/src/main/java/org/exoplatform/portal/pom/config/POMDataStorage.java
+++ component/portal/src/main/java/org/exoplatform/portal/pom/config/POMDataStorage.java
@@ -39,6 +39,7 @@ import org.exoplatform.portal.config.model.ModelObject;
 import org.exoplatform.portal.config.model.PersistentApplicationState;
 import org.exoplatform.portal.config.model.TransientApplicationState;
 import org.exoplatform.portal.pom.config.tasks.DashboardTask;
+import org.exoplatform.portal.pom.config.tasks.MOPAccess;
 import org.exoplatform.portal.pom.config.tasks.NodeTask;
 import org.exoplatform.portal.pom.config.tasks.PageNavigationTask;
 import org.exoplatform.portal.pom.config.tasks.PageTask;
@@ -250,11 +251,11 @@ public class POMDataStorage implements ModelDataStorage
       Class<T> type = q.getClassType();
       if (PageData.class.equals(type))
       {
-         return (LazyPageList<T>)pomMgr.execute(new SearchTask.FindPage((Query<PageData>)q));
+         return (LazyPageList<T>)new LazyPageList<PageData>(new MOPAccess.PageAccess(pomMgr, (Query<PageData>)q), 10);
       }
       else if (NavigationData.class.equals(type))
       {
-         return (LazyPageList<T>)pomMgr.execute(new SearchTask.FindNavigation((Query<NavigationData>)q));
+         return (LazyPageList<T>)new LazyPageList<NavigationData>(new MOPAccess.NavigationAccess(pomMgr, (Query<NavigationData>)q), 10);
       }
       else if (PortletPreferences.class.equals(type))
       {
